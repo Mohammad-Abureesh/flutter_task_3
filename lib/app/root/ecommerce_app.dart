4d9e4ef651628194c8bf5a275cf34db27ed5b0ab
Mohammad H Abureesh <m.abualreesh@pcnc2000.com>
2023-11-24 15:51:49 +0200

    Splash screen

--- lib/app/root/ecommerce_app.dart
+++ lib/app/root/ecommerce_app.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_task_3/core/presentation/style/app_style.dart';
+
+import 'splash_screen.dart';
 
 class EcommerceApp extends StatelessWidget {
   const EcommerceApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.light,
+        theme: LightTheme().theme,
+        home: const SplashScreen());
   }
 }
