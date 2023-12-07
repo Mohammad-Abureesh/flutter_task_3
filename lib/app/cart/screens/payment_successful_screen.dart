a8d9f88743ea2ca1697aa671c9d30646332f5bc5
Mohammad H Abureesh <m.abualreesh@pcnc2000.com>
2023-12-08 01:05:55 +0200

    Payment successful screen

---
+++ lib/app/cart/screens/payment_successful_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '/core/presentation/widgets/general_app_bar.dart';
+import '/core/presentation/widgets/submit_button.dart';
+import '/core/presentation/widgets/text_widget.dart';
+
+class PaymentSuccessfulScreen extends StatelessWidget {
+  const PaymentSuccessfulScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const SizedBox gap = SizedBox(height: 15.0);
+
+    return Scaffold(
+      appBar: GeneralAppBar(title: 'Payment'),
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.check_circle,
+                color: Theme.of(context).primaryColor, size: 60.0),
+            gap,
+            const TextWidget(data: 'Payment Successful!', size: 18.0),
+            const TextWidget(
+                data: 'Your order will be processed an sent to '
+                    'you as soon as possible'),
+            gap,
+            SubmitButton(
+                title: 'Continue Shopping',
+                onPressed: Navigator.of(context).pop)
+          ]),
+    );
+  }
+}
