b1c6f9c693d7820720b3fc88747774fe7846fae9
Mohammad H Abureesh <m.abualreesh@pcnc2000.com>
2023-12-08 01:25:57 +0200

    Update social login buttons icons

--- lib/app/registration/login/widgets/social_login_buttons.dart
+++ lib/app/registration/login/widgets/social_login_buttons.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 enum SocialLogin {
-  google(Icons.g_mobiledata_outlined),
-  facebook(Icons.facebook);
+  google(FontAwesomeIcons.google),
+  facebook(FontAwesomeIcons.facebookF);
 
   const SocialLogin(this.icon);
 
   final IconData icon;
+
+  Color? get iconColor => switch (this) { google => Colors.red, _ => null };
 }
 
 class SocialLoginButtons extends StatelessWidget {
@@ -28,8 +31,15 @@ class _SocialButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CircleAvatar(
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        child: Icon(value.icon));
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Material(
+        elevation: 4.0,
+        borderRadius: BorderRadius.circular(30.0),
+        child: CircleAvatar(
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            child: Icon(value.icon, color: value.iconColor)),
+      ),
+    );
   }
 }
