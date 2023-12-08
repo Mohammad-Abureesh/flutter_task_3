7d203a7acd03f0fd3b4753de47af33f23ada63a2
Mohammad H Abureesh <m.abualreesh@pcnc2000.com>
2023-12-08 02:55:47 +0200

    Add notification_entity.dart & notifications.json

---
+++ lib/core/domain/entities/notification_entity.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class NotificationEntity {
+  String title;
+  String description;
+
+  String? _date;
+
+  int? _iconLogo;
+
+  NotificationEntity(this.title, this.description, this._date, this._iconLogo);
+  factory NotificationEntity.fromJson(Map<String, dynamic> json) {
+    return NotificationEntity(json['title'] ?? '', json['description'] ?? '',
+        json['date'].toString(), json['logo']);
+  }
+
+  IconData get icon {
+    if (_iconLogo == null) return Icons.notifications;
+    return IconData(_iconLogo!, fontFamily: 'MaterialIcons');
+  }
+}
