3cff94adee38782b9233d5b21503b6c19c541294
Mohammad H Abureesh <m.abualreesh@pcnc2000.com>
2023-12-09 02:56:30 +0200

    Add cards background colors

--- lib/core/presentation/style/pallet.dart
+++ lib/core/presentation/style/pallet.dart
@@ -1,10 +1,13 @@
 part of app_style;
 
 abstract class Pallet {
-  static const primary = Color(0xff6F4BF2);
-  static const secondary = Color(0xff302559);
-  static const lightPurple = Color(0xff977EF2);
-  static const lightGrey = Color(0xffF2F2F2);
+  static const Color primary = Color(0xff6F4BF2);
+  static const Color secondary = Color(0xff302559);
+  static const Color lightPurple = Color(0xff977EF2);
+  static const Color lightGrey = Color(0xffF2F2F2);
+
+  static const Color lightBlack = Color(0xff0D0D0D);
+  static const Color silver = Color(0xffB4BBBF);
 
   static List<Color> get productColors {
     return [Colors.black, Colors.grey, secondary, primary];
